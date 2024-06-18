2be727dcbcb1b292c14d8c5187af87bb5bcee37e
Andrea <andreafernandez281@gmail.com>
2024-06-18 17:03:02 +0200

    Añadiendo PortfolioManager

---
+++ src/components/portfolio/portfolio-sidebar-list.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const PortfolioSideBarList = props => {
+    const PortfolioList = props.data
+    
+    return <div>List...</div>
+
+}
+
+export default PortfolioSideBarList;
