56e5e607f5f7c54386715212c0019d092f8d4100
Andrea <andreafernandez281@gmail.com>
2024-07-07 19:36:29 +0200

    Las entradas de blog en el index aparecen resumidas

--- src/components/blog/blog-item.js
+++ src/components/blog/blog-item.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import striptags from "striptags";
+import Truncate from "react-truncate";
 
 const BlogItem = (props) => {
   const { id, blog_status, content, title, featured_image_url } =
@@ -11,7 +13,18 @@ const BlogItem = (props) => {
         <h1>{title}</h1>
       </Link>
 
-      <div>{content}</div>
+      <div>
+        <Truncate
+          lines={5}
+          ellipsis={
+            <span>
+              ... <Link to={`/b/${id}`}>Read more</Link>
+            </span>
+          }
+        >
+          {striptags(content)}
+        </Truncate>
+      </div>
     </div>
   );
 };
